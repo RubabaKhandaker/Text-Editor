c28790ffe265823d14ce95afeb526ea09237b0d4
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-01-30 20:03:47 +1100

    Added code to webpack.config.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,7 +7,9 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
+
   return {
+
     mode: 'development',
     entry: {
       main: './src/js/index.js',
@@ -19,12 +21,79 @@ module.exports = () => {
     },
     plugins: [
       
+      new HtmlWebpackPlugin({
+
+        template: './index.html',
+        title: 'JATE',
+
+      }),
+      new InjectManifest({
+
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+
+      }),
+
+      new WebpackPwaManifest({
+
+        fingerprints:false,
+        inject:true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'As the name suggests.',
+        background_color: '#ff80c1',
+        theme_color: '#ff80c1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+
+          {
+
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+
+          },
+
+        ],
+
+      }),
+
     ],
 
     module: {
+
       rules: [
-        
+
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+
+            loader: 'babel-loader',
+            options: {
+
+              presets: ['@babel/preset-env'],
+              plugins: [
+
+                '@babel/plugin-proposal-object-rest-spread', 
+                '@babel/transform-runtime'
+              ],
+            },
+
+          },
+
+        },
+
       ],
+
     },
+
   };
+  
 };
